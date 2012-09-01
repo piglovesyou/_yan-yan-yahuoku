5103b085729d74b58a57e19411d5adebab8ed0c2
piglovesyou <thepiglovesyou@gmail.com>
2012-09-02 00:51:29 +0900

    container (split pane)

--- public/my/app/app.js
+++ public/my/app/app.js
@@ -6,6 +6,7 @@ goog.require('goog.events.EventType');
 goog.require('my.ui.ThousandRows');
 
 goog.require('my.ds.Xhr');
+goog.require('my.app.Container');
 goog.require('my.dom.ViewportSizeMonitor');
 
 
@@ -22,44 +23,13 @@ goog.inherits(my.App, goog.ui.Component);
 /** @inheritDoc */
 my.App.prototype.enterDocument = function () {
   goog.base(this, 'enterDocument');
-  var vsm = my.dom.ViewportSizeMonitor.getInstance()
-  this.getHandler()
-    .listen(vsm, goog.events.EventType.RESIZE, function (e) {
-      this.resetLayoutSize_(e.target.getSize());
-      this.thousandRows_.update();
-    });
-  this.resetLayoutSize_(vsm.getSize());
-
 };
 
-my.App.prototype.resetLayoutSize_ = function (size) {
-  goog.style.setHeight(this.containerElement_,
-      size.height - this.toolbarHeight_);
-};
-
-/**
- * @type {?goog.ui.ThousandRows}
- */
-my.App.prototype.thousandRows_;
 
 /** @inheritDoc */
 my.App.prototype.decorateInternal = function (element) {
-  this.initializeSize_();
-
-  this.thousandRows_ = new my.ui.ThousandRows(160, 50);
-  var model = new my.ui.ThousandRows.Model(
-                  this.thousandRows_.baseName + this.thousandRows_.getId(),
-                  '/api/search?query=ベアブリック+23', 10000, true);
-  this.thousandRows_.setModel(model);
-  this.thousandRows_.decorate(this.containerElement_);
-};
-
-/**
- * @type {?number}
- */
-my.App.prototype.toolbarHeight_;
-my.App.prototype.initializeSize_ = function () {
-  this.toolbarHeight_ = this.toolbarElement_.offsetHeight;
+  var container = new my.app.Container(this.getDomHelper());
+  container.decorate(this.containerElement_);
 };
 
 /** @inheritDoc */
@@ -67,8 +37,9 @@ my.App.prototype.canDecorate = function (element) {
   if (element) {
     var dh = this.getDomHelper();
     var toolbar = dh.getElementByClass('toolbar', element);
+    var searchbar = dh.getElementByClass('searchbar', element);
     var container = dh.getElementByClass('container', element);
-    if (toolbar && container) {
+    if (toolbar && searchbar && container) {
       this.toolbarElement_ = toolbar;
       this.containerElement_ = container;
       this.setElementInternal(element);
---
+++ public/my/app/container.js
@@ -0,0 +1,92 @@
+
+goog.provide('my.app.Container');
+
+goog.require('goog.ui.SplitPane');
+goog.require('goog.style');
+goog.require('my.ui.ThousandRows');
+
+
+/**
+ * @constructor
+ * @extends {goog.ui.SplitPane}
+ */
+my.app.Container = function (opt_domHelper) {
+
+  var thousandRows = this.createThousandRows_(opt_domHelper);
+
+  goog.base(this, thousandRows, new goog.ui.Component(), opt_domHelper);
+  this.setHandleSize(0);
+
+  /**
+   * @type {my.ui.ThousandRows}
+   */
+  this.thousandRows_ = thousandRows;
+}
+goog.inherits(my.app.Container, goog.ui.SplitPane);
+
+
+
+my.app.Container.prototype.enterDocument = function () {
+  this.getHandler()
+    .listen(my.dom.ViewportSizeMonitor.getInstance(), goog.events.EventType.RESIZE, function (e) {
+      this.resize_();
+      this.thousandRows_.update();
+    });
+  this.resize_();
+  goog.base(this, 'enterDocument');
+};
+
+/**
+ * @type {?number}
+ */
+my.app.Container.prototype.offsetTopCache_;
+
+/**
+ * @return {number}
+ */
+my.app.Container.prototype.getOffsetTop_ = function () {
+  return this.offsetTopCache_ || (this.offsetTopCache_ = this.getElement().offsetTop);
+};
+
+
+/**
+ * We don't use setSize() because we want to resize 
+ *    based on this.detailPaneElement_.offsetWidth.
+ */
+my.app.Container.prototype.resize_ = function () {
+  var size = my.dom.ViewportSizeMonitor.getInstance().getSize();
+  size.height -= this.getOffsetTop_();
+  goog.style.setBorderBoxSize(this.getElement(), size);
+  this.setFirstComponentSize(size.width - this.detailPaneElement_.offsetWidth);
+};
+
+
+my.app.Container.prototype.createThousandRows_ = function (opt_domHelper) {
+  var thousandRows = new my.ui.ThousandRows(160, 50, opt_domHelper);
+  var model = new my.ui.ThousandRows.Model(
+                  thousandRows.baseName + thousandRows.getId(),
+                  '/api/search?query=ベアブリック+23', 10000, true);
+  thousandRows.setModel(model)
+  return thousandRows;
+};
+
+my.app.Container.prototype.decorateInternal = function (element) {
+  this.thousandRows_.decorate(this.scrollerElement_);
+
+  goog.base(this, 'decorateInternal', element);
+};
+
+my.app.Container.prototype.canDecorate = function (element) {
+  if (goog.base(this, 'canDecorate', element)) {
+    var scrollerElement = goog.dom.getElementByClass('goog-scroller', element);
+    var detailPaneElement = goog.dom.getElementByClass('detail-pane', element);
+
+    if (scrollerElement) {
+      this.scrollerElement_ = scrollerElement;
+      this.detailPaneElement_ = detailPaneElement;
+      return true;
+    }
+  }
+};
+
+

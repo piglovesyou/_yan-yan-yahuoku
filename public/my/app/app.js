6693f27d4d385d95a5fc020f3d030296dee68053
piglovesyou <thepiglovesyou@gmail.com>
2012-09-03 03:05:34 +0900

    enable tab drag

--- public/my/app/app.js
+++ public/my/app/app.js
@@ -7,6 +7,7 @@ goog.require('my.ui.ThousandRows');
 
 goog.require('my.ds.Xhr');
 goog.require('my.app.Container');
+goog.require('my.app.Tabs');
 goog.require('my.dom.ViewportSizeMonitor');
 
 
@@ -28,6 +29,10 @@ my.App.prototype.enterDocument = function () {
 
 /** @inheritDoc */
 my.App.prototype.decorateInternal = function (element) {
+  
+  var tabs = new my.app.Tabs;
+  tabs.decorate(this.tabsElement_);
+
   var container = new my.app.Container(this.getDomHelper());
   container.decorate(this.containerElement_);
 };
@@ -37,10 +42,12 @@ my.App.prototype.canDecorate = function (element) {
   if (element) {
     var dh = this.getDomHelper();
     var toolbar = dh.getElementByClass('toolbar', element);
+    var tabs = dh.getElementByClass('tabs', element);
     var searchbar = dh.getElementByClass('searchbar', element);
     var container = dh.getElementByClass('container', element);
     if (toolbar && searchbar && container) {
       this.toolbarElement_ = toolbar;
+      this.tabsElement_ = tabs;
       this.containerElement_ = container;
       this.setElementInternal(element);
       return true;
---
+++ public/my/app/tabs.js
@@ -0,0 +1,142 @@
+
+goog.provide('my.app.Tabs');
+
+goog.require('goog.ui.Component');
+goog.require('goog.fx.DragListGroup');
+goog.require('goog.asserts');
+
+
+/**
+ * @param {goog.dom.DomHelper=} opt_domHelper
+ * @constructor
+ * @extends {goog.ui.Component}
+ */
+my.app.Tabs = function (opt_domHelper) {
+  goog.base(this, opt_domHelper);
+};
+goog.inherits(my.app.Tabs, goog.ui.Component);
+
+
+my.app.Tabs.prototype.tabElements_ = [];
+
+my.app.Tabs.prototype.decorateInternal = function (element) {
+  var dh = this.getDomHelper();
+  goog.array.forEach(dh.getChildren(this.contentElement_), function (tabElm) {
+    var tab = new my.app.Tabs.Tab(dh);
+    this.addChild(tab);
+    tab.decorate(tabElm);
+    if (goog.dom.classes.has(tabElm, 'selected')) {
+      goog.asserts.assert(!this.currSelectedTab_, 'Two or more selected tab element.');
+      this.currSelectedTab_ = tab;
+    }
+  }, this);
+  this.setupDragListGroup_();
+};
+
+
+/**
+ * @type {?my.app.Tabs.Tab}
+ */
+my.app.Tabs.prototype.currSelectedTab_;
+
+
+my.app.Tabs.prototype.draggingClassName_ = 'tab-dragging';
+my.app.Tabs.prototype.dragListGroup_;
+my.app.Tabs.prototype.setupDragListGroup_ = function () {
+  if (this.dragListGroup_) {
+    this.dragListGroup_.dispose();
+  }
+  this.dragListGroup_ = new goog.fx.DragListGroup;
+  // this.dragListGroup_.setDragItemHandleHoverClass('yeah', 'ohh');
+  this.dragListGroup_.setDraggerElClass(this.draggingClassName_);
+  this.dragListGroup_.addDragList(this.contentElement_, goog.fx.DragListDirection.RIGHT);
+  var styleSheetEl;
+  this.getHandler()
+    .listen(this.dragListGroup_, 'beforedragstart', function (e) {
+      styleSheetEl = this.createFixTabWidthStylesheet_(e.currDragItem.offsetWidth);
+    })
+    .listen(this.dragListGroup_, 'dragend', function (e) {
+      goog.style.uninstallStyles(styleSheetEl);
+
+      // Becuase of dragListGroup, tab cannot listen click event by itself.
+      // So, we take care of it by here.
+      var tab = this.findChildByElement_(e.currDragItem);
+      this.selectTab(tab);
+    })
+  this.dragListGroup_.init();
+};
+
+
+/**
+ * @param {my.app.Tabs.Tab} tab
+ */
+my.app.Tabs.prototype.selectTab = function (tab) {
+  if (this.currSelectedTab_.getId() == tab.getId()) return;
+  goog.dom.classes.enable(tab.getElement(), 'selected', true);
+  goog.dom.classes.enable(this.currSelectedTab_.getElement(), 'selected', false);
+  this.currSelectedTab_ = tab;
+};
+
+/**
+ * @param {Element} element
+ * @return {my.app.Tabs.Tab}
+ */
+my.app.Tabs.prototype.findChildByElement_ = function (element) {
+  var child;
+  goog.array.find(this.getChildIds(), function (id) {
+    var c = this.getChild(id);
+    if (c && element == c.getElement()) {
+      child = c;
+      return true;
+    }
+    return false;
+  }, this);
+  return child;
+};
+
+
+my.app.Tabs.prototype.createFixTabWidthStylesheet_ = function (width) {
+  var styleString = '.tabs-content > .tab,.' + 
+                    this.draggingClassName_ +
+                    '{width: ' + width + 'px;}';
+  return goog.style.installStyles(styleString)
+};
+
+my.app.Tabs.prototype.canDecorate = function (element) {
+  var content = goog.dom.getElementByClass('tabs-content', element);
+  if (content) {
+    this.contentElement_ = content;
+    this.setElementInternal(element);
+    return true;
+  }
+  return false;
+};
+
+/**
+ * @param {goog.dom.DomHelper=} opt_domHelper
+ * @constructor
+ * @extends {goog.ui.Component}
+ */
+my.app.Tabs.Tab = function (opt_domHelper) {
+  goog.base(this, opt_domHelper);
+};
+goog.inherits(my.app.Tabs.Tab, goog.ui.Component);
+
+/** @inheritDoc */
+my.app.Tabs.Tab.prototype.decorateInternal = function (element) {
+  this.setElementInternal(element);
+  goog.style.setUnselectable(element, true, true);
+};
+
+/** @inheritDoc */
+my.app.Tabs.Tab.prototype.canDecorate = function (element) {
+  if (element) {
+    var dh = this.getDomHelper();
+    var content = dh.getElementByClass('tab-content', element);
+    if (content) {
+      this.contentElement_ = content;
+      return true;
+    }
+  }
+  return false;
+};

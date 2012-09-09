19f4e32f4fca6824c7e63dae2cf2b5e863f753f6
piglovesyou <thepiglovesyou@gmail.com>
2012-09-09 13:08:58 +0900

    textboxes

--- public/my/app/searchbar/searchbar.js
+++ public/my/app/searchbar/searchbar.js
@@ -17,6 +17,10 @@ goog.inherits(my.app.Searchbar, goog.ui.Component);
 
 my.app.Searchbar.prototype.createDom = function () {
   var dh = this.getDomHelper();
-  var element = dh.createDom('div', 'searchbar', 'yeah');
+  var element = dh.createDom('div', 'searchbar',
+      dh.createDom('div', 'textsearch',
+        dh.createDom('input', { })),
+      dh.createDom('div', 'categorysearch',
+        dh.createDom('input', {})));
   this.setElementInternal(element);
 };

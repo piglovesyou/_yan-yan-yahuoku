6abcaf9dc9070d5abf895de77428c5e46fa402c1
Takamura Soichi <takamura_s@ariel-networks.com>
2014-04-03 00:06:28 +0000

    refactor

--- scripts/categorycollector2.js
+++ scripts/categorycollector2.js
@@ -11,10 +11,9 @@ var yahooGet = Q.denodeify(yahoo.get.bind(yahoo));
 
 
 
-var depth = 0;
-
-
 fetchFromIds(["0"]).then(console.log);
+
+
 
 function fetchFromIds(ids) {
   var childIds = [];
@@ -65,54 +64,6 @@ function extractChildIds(parent) {
   return null;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// request("0")
-// .catch(outError)
-// .done(function() {
-//   console.log('[done]', i, 'items.');
-// });
-// 
-// function request(id, parent, child) {
-//   return findOne({CategoryId: id})
-//   .then(function(doc) {
-//     if (doc) {
-//       console.log('[exists]', doc.CategoryPath, i++);
-//       // throw new Error(doc);
-//       return doc;
-//     } else {
-//       return yahooGet('categoryTree', {category: id})
-//       .then(JSON.parse)
-//       .get('ResultSet')
-//       .get('Result')
-//       .catch(outError)
-//       .then(upsertStore)
-//       .then(function(doc) {
-//         console.log('[insert]', doc.CategoryPath, i++);
-//         return doc;
-//       })
-//     }
-//   })
-//   .then(requestChildrenIfAny);
-// };
-
 function outError(reason) {
   throw new Error(err + '\n' + err.stack);
 }
@@ -122,19 +73,5 @@ function upsertStore(result) {
   .then(function() {
     return result
   });
-}
-
-function requestChildrenIfAny(parent) {
-  if (parent.IsLeaf == 'false' && parent.IsLink == 'false' && parent.ChildCategory.length > 0) {
-    return parent.ChildCategory.reduce(function(q, child) {
-      // var path = child.CategoryIdPath.split(',');
-      if (child.IsLink == 'false') {
-        return q.then(function() {
-          return request(child.CategoryId, parent, child);
-        });
-      }
-      return q;
-    }, Q());
-  }
 }
 

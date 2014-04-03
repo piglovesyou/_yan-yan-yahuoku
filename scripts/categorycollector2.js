a8bd8e81d6c9fbabc0d5b941482f5dff946b65ad
Takamura Soichi <takamura_s@ariel-networks.com>
2014-04-03 00:01:47 +0000

    width first category collector

---
+++ scripts/categorycollector2.js
@@ -0,0 +1,140 @@
+
+var Q = require('q');
+Q.longStackSupport = true;
+var yahoo = require('../sources/net/yahoo');
+var Datastore = require('nedb');
+var db = new Datastore({ filename: './nedb/categories.nedb', autoload: true });
+
+var update = Q.denodeify(db.update.bind(db));
+var findOne = Q.denodeify(db.findOne.bind(db));
+var yahooGet = Q.denodeify(yahoo.get.bind(yahoo));
+
+
+
+var depth = 0;
+
+
+fetchFromIds(["0"]).then(console.log);
+
+function fetchFromIds(ids) {
+  var childIds = [];
+  return ids.reduce(function(q, id) {
+    return q.then(fetchFromId.bind(null, id))
+    .then(function(cids) {
+      if (cids) childIds = childIds.concat(cids);
+    });
+  }, Q()).then(function() {
+    if (childIds.length > 0) {
+      return fetchFromIds(childIds)
+    }
+    return 'done!';
+  });
+}
+
+function fetchFromId(id, parent, child) {
+  return findOne({CategoryId: id})
+  .then(function(doc) {
+    if (doc) {
+      console.log('[exists]', doc.CategoryPath);
+      // throw new Error(doc);
+      return extractChildIds(doc);
+    } else {
+      return yahooGet('categoryTree', {category: id})
+      .then(JSON.parse)
+      .get('ResultSet')
+      .get('Result')
+      .catch(outError)
+      .then(upsertStore)
+      .then(function(doc) {
+        console.log('[insert]', doc.CategoryPath);
+        return extractChildIds(doc);
+      });
+    }
+  })
+};
+
+function extractChildIds(parent) {
+  if (parent.IsLeaf == 'false' && parent.IsLink == 'false' && parent.ChildCategory.length > 0) {
+    return parent.ChildCategory.reduce(function(arr, child) {
+      if (child.IsLink == 'false') {
+        arr.push(child.CategoryId);
+      }
+      return arr;
+    }, []);
+  }
+  return null;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// request("0")
+// .catch(outError)
+// .done(function() {
+//   console.log('[done]', i, 'items.');
+// });
+// 
+// function request(id, parent, child) {
+//   return findOne({CategoryId: id})
+//   .then(function(doc) {
+//     if (doc) {
+//       console.log('[exists]', doc.CategoryPath, i++);
+//       // throw new Error(doc);
+//       return doc;
+//     } else {
+//       return yahooGet('categoryTree', {category: id})
+//       .then(JSON.parse)
+//       .get('ResultSet')
+//       .get('Result')
+//       .catch(outError)
+//       .then(upsertStore)
+//       .then(function(doc) {
+//         console.log('[insert]', doc.CategoryPath, i++);
+//         return doc;
+//       })
+//     }
+//   })
+//   .then(requestChildrenIfAny);
+// };
+
+function outError(reason) {
+  throw new Error(err + '\n' + err.stack);
+}
+
+function upsertStore(result) {
+  return update({CategoryId: result.CategoryId}, result, {upsert: true})
+  .then(function() {
+    return result
+  });
+}
+
+function requestChildrenIfAny(parent) {
+  if (parent.IsLeaf == 'false' && parent.IsLink == 'false' && parent.ChildCategory.length > 0) {
+    return parent.ChildCategory.reduce(function(q, child) {
+      // var path = child.CategoryIdPath.split(',');
+      if (child.IsLink == 'false') {
+        return q.then(function() {
+          return request(child.CategoryId, parent, child);
+        });
+      }
+      return q;
+    }, Q());
+  }
+}
+

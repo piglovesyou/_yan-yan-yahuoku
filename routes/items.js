981e4033e6998fbdc03d89414a4db027481073b5
Takamura Soichi <takamura_s@ariel-networks.com>
2014-05-05 00:29:32 +0000

    refac

--- routes/items.js
+++ routes/items.js
@@ -25,29 +25,31 @@ function asYahooRequest(yahooPath) {
   return function(req, res) {
     yahooGet(yahooPath, req.query)
     .then(JSON.parse)
-    .then(function(data) {
-      // TODO: Add properties by using "string" utility.
-      var items = goog.getObjectByName('ResultSet.Result.Item', data);
-      if (items) {
-        _(items).each(function(i) {
-          if (i.CurrentPrice)
-              i.displayCurrentPrice = string.renderPrice(i.CurrentPrice);
-          if (i.EndTime) // Use "moment"
-              i.displayEndTime = string.renderEndDate(i.EndTime);
-        });
-      }
-      return data;
-    })
-    .then(function(data) {
+    .then(resolveDisplayValue)
+    .then(function(json) {
       res.status(200);
       res.contentType('application/json; charset=utf-8');
-      res.send(data);
+      res.send(json);
     })
     .catch(function(err) {
       outError(err);
       res.send({});
     });
   }
+}
+
+function resolveDisplayValue(json) {
+  // TODO: Add properties by using "string" utility.
+  var items = goog.getObjectByName('ResultSet.Result.Item', json);
+  if (items) {
+    _(items).each(function(i) {
+      if (i.CurrentPrice)
+          i.displayCurrentPrice = string.renderPrice(i.CurrentPrice);
+      if (i.EndTime) // Use "moment"
+          i.displayEndTime = string.renderEndDate(i.EndTime);
+    });
+  }
+  return json;
 }
 
 module.exports = router;

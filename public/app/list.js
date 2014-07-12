2100df3f9f8967cad81da58d4f5567999a5c9586
Takamura Soichi <takamura_s@ariel-networks.com>
2014-07-12 13:30:50 +0000

    fix: server handle yahoo items api nicely

--- public/app/list.js
+++ public/app/list.js
@@ -1,6 +1,5 @@
 
 goog.provide('app.List');
-goog.provide('app.list.Data');
 
 goog.require('app.soy.item');
 goog.require('goog.ui.List');
@@ -39,18 +38,18 @@ app.List.prototype.createDom = function() {
 /** @inheritDoc */
 app.List.prototype.handleTotalUpdate = function(e) {
   goog.base(this, 'handleTotalUpdate', e);
-  var data = /** @type {app.list.Data} */(e.target);
+  var data = /** @type {goog.ui.list.Data} */(e.target);
   goog.style.setElementShown(this.messageEl_, data.getTotal() == 0)
 };
 
 /**
  * @param {goog.Uri} url .
  * @param {number} rowCountPerPage .
- * @return {app.list.Data} .
+ * @return {goog.ui.list.Data} .
  */
 app.List.createData = function(url, rowCountPerPage) {
   // Url to request remote JSON Must be 20 because of Yahoo.
-  var data = new app.list.Data(url, rowCountPerPage);
+  var data = new goog.ui.list.Data(url, rowCountPerPage);
   data.setObjectNameTotalInJson('ResultSet.@attributes.totalResultsAvailable');
   data.setObjectNameRowsInJson('ResultSet.Result.Item');
   return data;
@@ -88,37 +87,3 @@ app.List.Item.prototype.renderContent = function(data) {
   goog.base(this, 'renderContent', data);
 };
 
-
-
-/**
- * @param {string|goog.Uri} url .
- * @param {number} rowCountPerPage .
- * @param {number=} opt_totalRowCount .
- * @param {boolean=} opt_keepTotalUptodate .
- * @param {goog.net.XhrManager=} opt_xhrManager .
- * @constructor
- * @extends {goog.ui.list.Data}
- */
-app.list.Data = function(url, rowCountPerPage,
-    opt_totalRowCount, opt_keepTotalUptodate, opt_xhrManager) {
-
-  /**
-   * We have to create "page" parameter..
-   */
-  this.rowCountPerPage = rowCountPerPage;
-
-  goog.base(this, url,
-    opt_totalRowCount, opt_keepTotalUptodate, opt_xhrManager);
-};
-goog.inherits(app.list.Data, goog.ui.list.Data);
-
-/** @inheritDoc */
-app.list.Data.prototype.buildUrl = function(from, count) {
-  var url = this.url.clone();
-
-  var pageIndex = from % this.rowCountPerPage ?
-      count : from / this.rowCountPerPage;
-
-  url.setParameterValue('page', pageIndex + 1);
-  return url.toString();
-};
--- routes/auction.js
+++ routes/auction.js
@@ -1,5 +1,6 @@
 
 var querystring = require('querystring');
+var path = require('path');
 var express = require('express');
 var router = express.Router();
 var Q = require('q');
@@ -13,32 +14,102 @@ var yahooGet = Q.denodeify(yahoo.get);
 var AFFILIATE_BASE = require('secret-strings').AUC_PRO.AFFILIATE_BASE;
 
 
+
 router.get('/', index);
-router.get('/search', asYahooRequest('search', itemsResolver));
-router.get('/categoryLeaf', asYahooRequest('categoryLeaf', itemsResolver));
-router.get('/auctionItem', asYahooRequest('auctionItem', detailResolver));
+router.get('/search', handleItemsRequest);
+router.get('/categoryLeaf', handleItemsRequest);
+router.get('/auctionItem', handleDetailRequest);
 
 
+
+function handleDetailRequest(req, res) {
+  yahooGet('auctionItem', req.query)
+  .then(JSON.parse)
+  .then(detailResolver)
+  .then(getJsonResponseStep(res))
+  .catch(getErrorResponseStep(res));
+}
+
+function getJsonResponseStep(res) {
+  return function (json) {
+    res.status(200);
+    res.contentType('application/json; charset=utf-8');
+    res.send(json);
+  }
+}
+
+function getErrorResponseStep(res) {
+  return function (err) {
+    outError(err);
+    res.send({});
+  }
+}
 
 function index(req, res) {
   res.send('respond with a resource');
 }
 
-function asYahooRequest(yahooPath, resolver) {
-  return function(req, res) {
-    yahooGet(yahooPath, req.query)
+function handleItemsRequest(req, res) {
+  var endpoint = path.basename(req.route.path);
+  Q.all(calcPages(+req.query.offset, +req.query.count).map(function(page) {
+    var dest = {};
+    goog.object.extend(dest, req.query, {page: page});
+    return yahooGet(endpoint, dest)
     .then(JSON.parse)
-    .then(resolver)
-    .then(function(json) {
-      res.status(200);
-      res.contentType('application/json; charset=utf-8');
-      res.send(json);
-    })
-    .catch(function(err) {
-      outError(err);
-      res.send({});
-    });
+    .then(itemsResolver);
+  })).then(mergeItemsResponse)
+  .then(getJsonResponseStep(res))
+  .catch(getErrorResponseStep(res));
+}
+
+function mergeItemsResponse(results) {
+  var dest = results[0];
+  var destMeta = goog.getObjectByName('ResultSet.@attributes', results[0]);
+  var destResult = goog.getObjectByName('ResultSet.Result', results[0]);
+  destMeta.totalResultsReturned = +destMeta.totalResultsReturned;
+  for (var i = 1; i < results.length; i++) {
+    var meta = goog.getObjectByName('ResultSet.@attributes', results[i]);
+    var items = goog.getObjectByName('ResultSet.Result.Item', results[i]);
+    destMeta.totalResultsReturned += +meta.totalResultsReturned;
+    destResult.Item = destResult.Item.concat(items);
   }
+  return dest;
+}
+
+/** @return {Promise} */
+function getYahooList(offset, count) {
+  return Q.all(calcPages(+req.query.offset, +req.query.count).map(function(page) {
+    var dest = {};
+    goog.object.extend(dest, req.query, {page: page});
+    return yahooGet(endpoint, dest)
+    .then(JSON.parse)
+    .then(resolver);
+  })).then(mergeYahooListResult);
+}
+
+function mergeYahooListResult(results) {
+  var dest = results[0];
+  var destMeta = goog.getObjectByName('ResultSet.@attributes', results[0]);
+  var destResult = goog.getObjectByName('ResultSet.Result', results[0]);
+  destMeta.totalResultsReturned = +destMeta.totalResultsReturned;
+  for (var i = 1; i < results.length; i++) {
+    var meta = goog.getObjectByName('ResultSet.@attributes', results[i]);
+    var items = goog.getObjectByName('ResultSet.Result.Item', results[i]);
+    destMeta.totalResultsReturned += +meta.totalResultsReturned;
+    destResult.Item = destResult.Item.concat(items);
+  }
+  return dest;
+}
+
+var PER_PAGE = 20;
+function calcPages(offset, count) {
+  var pages = [];
+  var last;
+  pages.push(last = offset / PER_PAGE + 1);
+  while ((count -= PER_PAGE) > 0) {
+    pages.push(++last);
+  }
+  return pages;
 }
 
 function detailResolver(json) {
